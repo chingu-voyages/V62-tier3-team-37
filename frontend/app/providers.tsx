20761bcf7fd8e31b44759cd89a60132645b4f544
Crocoboy000 <ragedofy@gmail.com>
2026-09-18 17:26:13 +0100

    chor:setup zustend package for state managment, plus tanstack query

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  }
+  if (!browserQueryClient) {
+    browserQueryClient = makeQueryClient();
+  }
+  return browserQueryClient;
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => getQueryClient());
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
